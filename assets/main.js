f291bd50de12fb4955e041343afaaa813b65f4a6
Orieta Toro <orieta.toro@gmail.com>
2017-05-04 11:01:18 -0300

    Ejercicio Pokemon final

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,27 @@
 function pokemon(nombre, color, poderDeAtaque){
-	this.nombre = nombre
-	this.color = color
+	this.nombre = nombre;
+	this.color = color;
 
-	this.nivelDeAmistad = 0
+	this.nivelDeAmistad = 0;
 
-	this.vida = 100
-	this.poderDeAtaque = poderDeAtaque
+	this.vida = 100;
+	this.poderDeAtaque = poderDeAtaque;
 	this.mostrarPokemon = function(){
-		return('Hola, soy: '+ this.nombre + ' y soy de color: '+ this.color)
+		return('Hola, soy: '+ this.nombre + ' y soy de color: '+ this.color);
 	}
 	this.aumentarAmistad = function(valor){
-		this.nivelDeAmistad = this.nivelDeAmistad + valor
+		this.nivelDeAmistad = this.nivelDeAmistad + valor;
 	}
 	this.atacar = function(pokemon){
-		pokemon.vida = pokemon.vida - this.poderDeAtaque
+		pokemon.vida = pokemon.vida - this.poderDeAtaque;
 	}
 }
+const Pikachu = new pokemon('Pikachu', 'amarillo', '100')
+const Charmander = new pokemon('Charmander', 'rojo', '20')
+
+Pikachu.atacar(Charmander)
+
+
+document.write(Charmander.nombre + " " + Charmander.color + " " + Charmander.vida + " " + " - " );
+document.write(Pikachu.nombre + " " + Pikachu.color + " "+ Pikachu.vida + " ");
+
